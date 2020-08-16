a578f5b2761386367f9c5ce5a88f8d5f93d5f54a
Krzysztof Darasz <krzysztof.darasz@brightpearl.com>
2020-08-16 18:55:11 +0100

    channels mpsc implementation

---
+++ src/lib.rs
@@ -0,0 +1,123 @@
+use std::sync::{Arc, Mutex, Condvar};
+use std::collections::VecDeque;
+
+pub struct Sender<T> {
+    shared: Arc<Shared<T>>
+}
+
+struct Shared<T> {
+    inner: Mutex<Inner<T>>,
+    available: Condvar,
+}
+
+struct Inner<T> {
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
+
+        Sender {
+            shared: Arc::clone(&self.shared)
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let was_last = inner.senders == 0;
+        drop(inner);
+        if was_last {
+            self.shared.available.notify_one();
+        }
+    }
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.queue.push_back(t);
+        drop(inner);
+        self.shared.available.notify_one();
+    }
+}
+
+pub struct Receiver<T> {
+    shared: Arc<Shared<T>>,
+    buffer: VecDeque<T>
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> Option<T> {
+
+        if let Some(t) = self.buffer.pop_front() {
+            return Some(t)
+        }
+
+        let mut inner = self.shared.inner.lock().unwrap();
+        loop {
+            match inner.queue.pop_front() {
+                Some(t) => {
+                    if !inner.queue.is_empty() {
+                        std::mem::swap(&mut inner.queue, &mut self.buffer);
+                    }
+                    return Some(t)
+                },
+                None if inner.senders == 0 => return None,
+                None => {
+                    inner = self.shared.available.wait(inner).unwrap()
+                }
+            }
+        }
+    }
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner{
+        queue: VecDeque::default(),
+        senders: 1,
+    };
+    let shared = Shared { inner: Mutex::new(inner), available: Condvar::new() };
+    let shared = Arc::new(shared);
+    (
+        Sender {
+            shared: shared.clone()
+        },
+        Receiver {
+            shared: shared.clone(),
+            buffer: VecDeque::default()
+        }
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::channel;
+
+    #[test]
+    fn it_works() {
+        let (mut tx, mut rx) = channel();
+        tx.send(42);
+        assert_eq!(rx.recv(), Some(42))
+    }
+
+    #[test]
+    fn should_be_closed(){
+        let (tx, mut rx) = channel::<u8>();
+        drop(tx);
+        assert_eq!(rx.recv(), None);
+    }
+
+    #[test]
+    fn closed_rx(){
+        let (mut tx, rx) = channel::<u8>();
+        drop(rx);
+        tx.send(42);
+    }
+}
